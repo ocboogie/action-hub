17ecc292e10322bfa0a60e72a8dd43d58bbd69cd
unknown <boogie@mikulec.com>
2017-06-17 15:13:23 -0700

    Improved gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,42 @@ watch = require('gulp-watch')
 replace = require('gulp-replace')
 plumber = require('gulp-plumber');
 
-const jsFiles = 'src/**/*.js'
-resourceFiles = 'src/**/*.?(html|eot|svg|ttf|woff|woff2)';
+const gulpTasks = {
+    js: {
+        taskName: 'js',
+        files: 'src/**/*.js'
+    },
+    resources: {
+        taskName: 'resources',
+        files: 'src/**/*.?(html|eot|svg|ttf|woff|woff2)'
+    }
+}
 
+gulp.task(gulpTasks.resources.taskName, () => {
+    return gulp.src(gulpTasks.resources.files)
+        .pipe(plumber())
+        .pipe(gulp.dest('dist'));
+});
 
-gulp.task('js', () => {
-    return gulp.src(jsFiles)
+gulp.task(gulpTasks.js.taskName, () => {
+    return gulp.src(gulpTasks.js.files)
         .pipe(plumber())
         .pipe(replace('__DEV__', (process.env.NODE_ENV === 'production') ? 'false' : 'true'))
         .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist'))
-});
+})
 
-gulp.task('resources', () => {
-    return gulp.src(resourceFiles)
-        .pipe(plumber())
-        .pipe(gulp.dest('dist'));
-});
+gulp.task('build', () => {
+    for (let value in gulpTasks) {
+        gulp.start(gulpTasks[value].taskName);
+    }
+})
 
 gulp.task('watch', () => {
-    gulp.watch(resourceFiles, ['resources']);
-    gulp.watch(jsFiles, ['js']);
+    for (let value in gulpTasks) {
+        gulp.watch(gulpTasks[value].files, [gulpTasks[value].taskName]);
+    }
+    // gulp.watch(jsFiles, ['js']);
 });
