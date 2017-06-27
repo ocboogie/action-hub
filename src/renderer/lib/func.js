05e94a4bdeb1b5b4f94b4102e03fb32b2f92ddb4
unknown <boogie@mikulec.com>
2017-06-26 20:59:29 -0700

    Improved func args handling

--- src/renderer/lib/func.js
+++ src/renderer/lib/func.js
@@ -5,40 +5,43 @@ import * as fsp from 'fs-promise';
 import replaceAll from '../../utills/replaceAll';
 
 const funcs = {
-    dir2actions: function dir2actions(args) {
-        if (args.path === undefined) {
-            // TODO
-            console.log('error');
-            return;
-        }
-        args.recursive = args.recursive || true;
-        args.flat = args.flat || true;
-        args.hideExtension = args.hideExtension || true;
-
-        let actions = [];
-        const files = fsp.readdirSync(args.path);
-        for (const file of files) {
-            const pathOfFile = path.join(args.path, file);
-            if (fsp.statSync(pathOfFile).isDirectory()) {
-                if (args.recursive) {
-                    if (args.flat) {
-                        actions = actions.concat(dir2actions({ ...args, path: pathOfFile }));
+    /* eslint-disable func-name-matching */
+    dir2actions: {
+        mandatoryArgs: [
+            'path'
+        ],
+        args: {
+            recursive: true,
+            flat: true,
+            hideExtension: true
+        },
+        func: function dir2actions(args) {
+            let actions = [];
+            const files = fsp.readdirSync(args.path);
+            for (const file of files) {
+                const pathOfFile = path.join(args.path, file);
+                if (fsp.statSync(pathOfFile).isDirectory()) {
+                    if (args.recursive) {
+                        if (args.flat) {
+                            actions = actions.concat(dir2actions({ ...args, path: pathOfFile }));
+                        } else {
+                            // TODO
+                        }
+                    }
+                } else {
+                    const fileName = (args.hideExtension) ? path.basename(pathOfFile).replace(/\.[^/.]+$/, '') : path.basename(pathOfFile);
+                    const action = { type: 'app', value: pathOfFile, text: fileName };
+                    if (args.container) {
+                        actions.push(replaceAll(args.container, '<action>', action));
                     } else {
-                        // TODO
+                        actions.push(action);
                     }
                 }
-            } else {
-                const fileName = (args.hideExtension) ? path.basename(pathOfFile).replace(/\.[^/.]+$/, '') : path.basename(pathOfFile);
-                const action = { type: 'app', value: pathOfFile, text: fileName };
-                if (args.container) {
-                    actions.push(replaceAll(args.container, '<action>', action));
-                } else {
-                    actions.push(action);
-                }
             }
+            return actions;
         }
-        return actions;
     }
+    /* eslint-enable func-name-matching */
 };
 
 // eslint-disable-next-line import/prefer-default-export
@@ -55,7 +58,14 @@ export function compileFuncs(obj) {
                 console.log('error');
                 return;
             }
-            return funcs[obj.funcName](obj.args || {});
+            for (const value of funcs[obj.funcName].mandatoryArgs) {
+                if (obj.args[value] === undefined) {
+                    // TODO
+                    console.log('error');
+                    return;
+                }
+            }
+            return funcs[obj.funcName].func(Object.assign(obj.args, funcs[obj.funcName].args));
         }
     }
 }
