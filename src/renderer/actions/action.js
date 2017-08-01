5395daac291199775e954da5caa6108ed32307fe
ocboogie <boogie@mikulec.com>
2017-07-31 19:44:06 -0700

    Actions can be lists now

--- src/renderer/actions/action.js
+++ src/renderer/actions/action.js
@@ -6,6 +6,12 @@ import displayError from '../displayError';
 // eslint-disable-next-line import/prefer-default-export
 export function runAction(action) {
     return dispatch => {
+        if (Array.isArray(action)) {
+            for (const oneAction of action) {
+                dispatch(runAction(oneAction));
+            }
+            return;
+        }
         if (!(action.type in actionMap)) {
             displayError(`Corrupt action: "${action.type}" is not a action type. Full action: [${action}]`);
             return;
