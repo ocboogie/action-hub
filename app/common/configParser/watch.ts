e88d896ad5d099d70ae7b53fb6f9ffe9a9881194
ocboogie <boogie@mikulec.com>
2017-11-12 20:01:09 -0800

    Fix promise error in watch.ts

--- app/common/configParser/watch.ts
+++ app/common/configParser/watch.ts
@@ -4,8 +4,6 @@ import parseFile from "./parseFile";
 
 export default function(configPath: string, listener: (object) => undefined) {
   chokidar.watch(configPath).on("change", () => {
-    parseFile(configPath).then(config => {
-      listener(config);
-    });
+    listener(parseFile(configPath));
   });
 }
