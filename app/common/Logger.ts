14e122673d6887abd2607b24f11560b5599517ce
Eamon mikulec <boogie@mikulec.com>
2017-12-06 22:01:00 -0800

    In logger change categories to an object

--- app/common/Logger.ts
+++ app/common/Logger.ts
@@ -9,16 +9,16 @@ export default class Logger {
   public history: ILoggedError[] = [];
   public historyLimit = 500;
 
-  private categories: Map<string, IListener[]> = new Map();
+  private categories: { [key: string]: IListener[] } = Object.create(null);
 
   constructor(categories: string[]) {
     categories.forEach(category => {
-      this.categories.set(category, []);
+      this.categories[category] = [];
     });
   }
 
   public hasCategory(category: string): boolean {
-    return this.categories.has(category);
+    return Object.prototype.hasOwnProperty.call(this.categories, category);
   }
 
   public addListener(category: string, listener: IListener): () => void {
@@ -45,7 +45,7 @@ export default class Logger {
   }
 
   private findCategory(category: string): IListener[] {
-    const foundCategory = this.categories.get(category);
+    const foundCategory = this.categories[category];
     if (!foundCategory) {
       throw new TypeError(`No category named "${category}"`);
     }
