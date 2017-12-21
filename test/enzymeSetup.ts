abb77709d95198d70ab830e6a90b30ad4c3d04e2
Eamon mikulec <boogie@mikulec.com>
2017-12-20 18:38:33 -0800

    Add support for enzyme

---
+++ test/enzymeSetup.ts
@@ -0,0 +1,5 @@
+// tslint:disable-next-line:no-implicit-dependencies
+import * as Enzyme from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+
+Enzyme.configure({ adapter: new Adapter() });
